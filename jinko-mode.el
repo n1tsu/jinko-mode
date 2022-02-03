60729ccbdf91686bce7726a97381dbff0a3d3721
Augustin Thiercelin <augustin.thiercelin@outlook.com>
2022-02-03 17:54:27 +0100

    init: add jink-mode.el file

---
+++ jinko-mode.el
@@ -0,0 +1,22 @@
+;;; jinko-mode.el --- Major mode for editing Jinko -*- coding: utf-8; lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Augustin Thiercelin
+;;
+;; Author: Augustin Thiercelin <https://github.com/n1tsu>
+;; Maintainer: Augustin Thiercelin <augustin.thiercelin@outlook.com>
+;; Created: February 02, 2022
+;; Modified: February 02, 2022
+;; Version: 1.0.0
+;; Keywords: languages jinko
+;; Homepage: https://github.com/n1tsu/jinko-mode
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Major mode for Jinko
+;;
+;;; Code:
+
+;;; jinko-mode.el ends here
