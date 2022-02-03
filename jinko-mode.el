2caf96aac1048146b2d5fba8b7846d5a2dbfc3c9
Augustin Thiercelin <augustin.thiercelin@outlook.com>
2022-02-03 19:27:50 +0100

    jinko-mode: add syntax table and comments handling

--- jinko-mode.el
+++ jinko-mode.el
@@ -35,8 +35,19 @@
       '(("incl\\|func\\|mut\\|for\\|in\\|if\\|while\\|test\\|loop" . 'font-lock-keyword-face)
         ("float\\|int\\|string\\|void" . 'font-lock-type-face)))
 
+(defvar jinko-mode-syntax-table nil "Syntax table for `jinko-mode'.")
+(setq jinko-mode-syntax-table
+      (let ((syn-table (make-syntax-table)))
+        (modify-syntax-entry ?\/ ". 124" syn-table)
+        (modify-syntax-entry ?* ". 23b" syn-table)
+        (modify-syntax-entry ?\n ">" syn-table)
+        (modify-syntax-entry ?# "<" syn-table)
+        syn-table))
+
 (define-derived-mode jinko-mode fundamental-mode "Jinko"
   "major mode for editing jinko language code."
+  (setq comment-start "// ") ;; FIXME Might actually be '# '
+  (setq comment-end "")
   (setq font-lock-defaults '(jinko-highlights)))
 
 (add-to-list 'auto-mode-alist '("\\.jk\\'" . jinko-mode))
